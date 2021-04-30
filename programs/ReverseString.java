35b2846cd67e62f4dd418c8cc4f93bd8fc4fee6f
Abhishek Kumar <>
2021-04-30 15:15:29 +0000

    reverse string

---
+++ programs/ReverseString.java
@@ -0,0 +1,12 @@
+class ReverseString {
+  public static void main(String[] args) {
+		String name="avishek";
+		int lang=name.length();
+				
+				String rev="";
+				for(int i=lang-1; i>=0; i--){
+						rev= rev+name.charAt(i);
+			}
+			System.out.println(rev);
+		}
+}
