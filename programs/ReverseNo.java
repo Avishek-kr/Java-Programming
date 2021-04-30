e9ef0c87988c0a18897c293eb5d440fdb90d218e
Abhishek Kumar <>
2021-04-30 15:03:58 +0000

    Reverse Number

---
+++ programs/ReverseNo.java
@@ -0,0 +1,11 @@
+class ReverseNo {
+  public static void main(String[] args) {
+			int rem,rev=0,no=5432;
+			while(no!=0){
+					rem=no%10;
+					rev=rev*10+rem;
+					no=no/10;
+			}
+			System.out.println(rev);
+		}
+}
